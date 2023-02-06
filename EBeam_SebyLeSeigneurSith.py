b3623bb918865dda475c35110353ac0016e55f35
SebyLeDanger <leni@insa-toulouse.fr>
2023-02-06 18:14:54 +0100

    correctWaveguide

--- EBeam_SebyLeSeigneurSith.py
+++ EBeam_SebyLeSeigneurSith.py
@@ -11,7 +11,7 @@ def MyMZI(DeltaL: int):
     topToBottom = mzi.size_info.se
 
     # Add two fiber grating couplers
-    mziWithGratingCouplers = pdk.add_fiber_array(fanout_length  = topToBottom[1]*0.9986+1, straight_separation = 0, name = "MZI"+str(DeltaL), component = mzi, grating_coupler = pdk.ebeam_gc_te1550, with_loopback = False, gc_port_labels = ["out", "opt_in_TE_1550_SebyleSeigneurSith_MZI" + str(DeltaL)], layer_label = (10,0))
+    mziWithGratingCouplers = pdk.add_fiber_array(fanout_length  = topToBottom[1]+1, straight_separation = 0, name = "MZI"+str(DeltaL), component = mzi, grating_coupler = pdk.ebeam_gc_te1550, with_loopback = False, gc_port_labels = ["out", "opt_in_TE_1550_SebyleSeigneurSith_MZI" + str(DeltaL)], layer_label = (10,0))
     
     return mziWithGratingCouplers
 
